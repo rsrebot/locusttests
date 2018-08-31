16efb039cd03302a7a4ae8ded72a772f41e91203
Roberto Srebot <roberto.srebot@globallogic.com>
2018-08-31 09:26:27 -0300

    Revert "initial commit"
    This reverts commit ab63c61383872509ee70f7225d3225461505487e.

--- runtest.ps1
+++
@@ -1,2 +0,0 @@
-#locust --host=https://ygnomallac.execute-api.us-east-1.amazonaws.com --logfile=locustfile.log
-locust --host=https://ffp90tgy14.execute-api.us-east-1.amazonaws.com --logfile=locustfile.log
